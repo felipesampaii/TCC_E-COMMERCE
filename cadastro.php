502d96a909ecc56201d244110b7d14c8d7c1ac20
felipesampaii <felipe.galdino.sampaio@gmail.com>
2023-12-05 20:01:23 -0300

    Pagina de cadastro de cliente

--- cadastro.php
+++ cadastro.php
@@ -2,7 +2,7 @@
 include 'conexao.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Dados comuns para ambas as pessoas
+    // Dados comuns para ambas as pessoas (pessoa e tipo_pessoa)
     $email = $_POST["email"];
     $senha = $_POST["senha"];
     $tipo_pessoa = $_POST["tipo_pessoa"];
@@ -14,19 +14,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obter o ID da pessoa recém-inserida
     $id_pessoa = $mysqli->insert_id;
 
-    //------------------------------------------------------------------------------------------------------
-    // Verifique o tipo de pessoa selecionado para o campo estado
-    if ($tipo_pessoa == 1) {
-        $estado_pf = $_POST["estado_pf"];
-    } 
-    elseif ($tipo_pessoa == 2) {
-        $estado_pj = $_POST["estado_pj"];
-    }
-    
+    // Obter o ID da pessoa recém-inserida
+    $id_pessoa = $mysqli->insert_id;
 
-    //------------------------------------------------------------------------------------------------------
     // Inserir dados de telefone
-    if ($tipo_pessoa == 1) {
+    if ($tipo_pessoa == 1) { // Pessoa Física
         $telefone_pf_ddd = $_POST["telefone_pf_ddd"];
         $telefone_pf_numero = $_POST["telefone_pf_numero"];
 
@@ -34,8 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $sql_telefone_pf = "INSERT INTO telefone (ddd, numero, id_pessoa)
                             VALUES ('$telefone_pf_ddd', '$telefone_pf_numero', $id_pessoa)";
         $mysqli->query($sql_telefone_pf);
-    } 
-    elseif ($tipo_pessoa == 2) { // Pessoa Jurídica
+    } elseif ($tipo_pessoa == 2) { // Pessoa Jurídica
         $telefone_pj_ddd = $_POST["telefone_pj_ddd"];
         $telefone_pj_numero = $_POST["telefone_pj_numero"];
 
@@ -45,39 +36,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mysqli->query($sql_telefone_pj);
     }
 
-    //-----------------------------------------------------------------------------------------------------------------
-    // tabela endereço 
-    if ($tipo_pessoa == 1) { // Pessoa Física
-        $logradouro_pf = $_POST["logradouro_pf"];
-        $estado_pf = $_POST["estado_pf"];
-        $cidade_pf = $_POST["cidade_pf"];
-        $bairro_pf = $_POST["bairro_pf"];
-        $cep_pf = $_POST["cep_pf"];
-        $numero_pf = $_POST["numero_pf"];
-        $complemento_pf = $_POST["complemento_pf"];
-    
-        // Inserir dados na tabela endereco para Pessoa Física
-        $sql_endereco_pf = "INSERT INTO endereco (logradouro, estado, cidade, bairro, cep, numero, complemento, id_pessoa)
-                            VALUES ('$logradouro_pf', '$estado_pf', '$cidade_pf', '$bairro_pf', '$cep_pf', '$numero_pf', '$complemento_pf', $id_pessoa)";
-        $mysqli->query($sql_endereco_pf);
-    } 
-    elseif ($tipo_pessoa == 2) { // Pessoa Jurídica
-        $logradouro_pj = $_POST["logradouro_pj"];
-        $estado_pj = $_POST["estado_pj"];
-        $cidade_pj = $_POST["cidade_pj"];
-        $bairro_pj = $_POST["bairro_pj"];
-        $cep_pj = $_POST["cep_pj"];
-        $numero_pj = $_POST["numero_pj"];
-        $complemento_pj = $_POST["complemento_pj"];
-    
-        // Inserir dados na tabela endereco para Pessoa Jurídica
-        $sql_endereco_pj = "INSERT INTO endereco (logradouro, estado, cidade, bairro, cep, numero, complemento, id_pessoa)
-                            VALUES ('$logradouro_pj', '$estado_pj', '$cidade_pj', '$bairro_pj', '$cep_pj', '$numero_pj', '$complemento_pj', $id_pessoa)";
-        $mysqli->query($sql_endereco_pj);
-    }
-
-//--------------------------------------------------------------------------------------------------------------------
-// tabelas pessoa fisica e pessoa juridica
     if ($tipo_pessoa == 1) { // Pessoa Física
         $nome_pf = $_POST["nome_pf"];
         $sobrenome_pf = $_POST["sobrenome_pf"];
@@ -87,10 +45,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Inserir dados na tabela pessoa_fisica
         $sql_pf = "INSERT INTO pessoa_fisica (nome_pf, sobrenome_pf, nascimento_pf, cpf_pf, id_pessoa)
                     VALUES ('$nome_pf', '$sobrenome_pf', '$nascimento_pf', '$cpf_pf', $id_pessoa)";
-
         $mysqli->query($sql_pf);
     }
-    elseif ($tipo_pessoa == 2) { // Pessoa Jurídica
+     elseif ($tipo_pessoa == 2) { // Pessoa Jurídica
         $nome_fantasia_pj = $_POST["nome_fantasia_pj"];
         $razao_social_pj = $_POST["razao_social_pj"];
         $cnpj_pj = $_POST["cnpj_pj"];
@@ -100,7 +57,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Inserir dados na tabela pessoa_juridica
         $sql_pj = "INSERT INTO pessoa_juridica (nome_fantasia_pj, razao_social_pj, cnpj_pj, abertura_pj, funcionario_comprador_pj, id_pessoa)
                     VALUES ('$nome_fantasia_pj', '$razao_social_pj', '$cnpj_pj', '$abertura_pj', '$funcionario_comprador_pj', $id_pessoa)";
-
         if(!$mysqli->query($sql_pj)){
             $erroCadastro = true;
         }
@@ -108,8 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($erroCadastro) {
         echo "Erro ao cadastrar os dados. Por favor, tente novamente.";
-    } 
-    else {
+    } else {
         // Redirecionar para a página de login
         header('Location: login.php');
         exit;
@@ -118,6 +73,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 else {
     $erroCadastro = true;
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -129,9 +85,6 @@ else {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
-     <script src="jquery.mask.js"></script>
 </head>
 
 <body>
@@ -163,7 +116,7 @@ else {
 <!-------------------------------------------------------------------------------------------------->
 
     <h1>Criar conta</h1>
-    <form method="POST" action="" onsubmit="return validarSenha();">
+    <form method="POST" action="">
         <label>Tipo de Pessoa:</label></br>
         <input type="radio" name="tipo_pessoa" value="1" required onclick="showFields('pessoa_fisica_fields')"> Pessoa Física
         <input type="radio" name="tipo_pessoa" value="2" required onclick="showFields('pessoa_juridica_fields')"> Pessoa Jurídica<br>
@@ -185,52 +138,6 @@ else {
             <label for="telefone_pf">Telefone:</label>
             <input type="text" name="telefone_pf_ddd" placeholder="DDD" maxlength="3">
             <input type="text" name="telefone_pf_numero" placeholder="Número" maxlength="11"><br>
-
-            <label for="estado_pf">Estado:</label>
-            <select name="estado_pf">
-                <option value="AC">Acre (AC)</option>
-                <option value="AL">Alagoas (AL)</option>
-                <option value="AP">Amapá (AP)</option>
-                <option value="AM">Amazonas (AM)</option>
-                <option value="BA">Bahia (BA)</option>
-                <option value="CE">Ceará (CE)</option>
-                <option value="DF">Distrito Federal (DF)</option>
-                <option value="ES">Espírito Santo (ES)</option>
-                <option value="GO">Goiás (GO)</option>
-                <option value="MA">Maranhão (MA)</option>
-                <option value="MT">Mato Grosso (MT)</option>
-                <option value="MS">Mato Grosso do Sul (MS)</option>
-                <option value="MG">Minas Gerais (MG)</option>
-                <option value="PA">Pará (PA)</option>
-                <option value="PB">Paraíba (PB)</option>
-                <option value="PR">Paraná (PR)</option>
-                <option value="PE">Pernambuco (PE)</option>
-                <option value="PI">Piauí (PI)</option>
-                <option value="RJ">Rio de Janeiro (RJ)</option>
-                <option value="RN">Rio Grande do Norte (RN)</option>
-                <option value="RS">Rio Grande do Sul (RS)</option>
-                <option value="RO">Rondônia (RO)</option>
-                <option value="RR">Roraima (RR)</option>
-                <option value="SC">Santa Catarina (SC)</option>
-                <option value="SP">São Paulo (SP)</option>
-                <option value="SE">Sergipe (SE)</option>
-                <option value="TO">Tocantins (TO)</option>
-            </select><br>
-
-            <label for="bairro_pf">Bairro:</label>
-            <input type="text" name="bairro_pf"><br>
-
-            <label for="cep_pf">CEP:</label>
-            <input type="text" name="cep_pf"><br>
-
-            <label for="logradouro_pj">Logradouro:</label>
-            <input type="text" name="logradouro_pj"><br>
-
-            <label for="numero_pf">Número:</label>
-            <input type="text" name="numero_pf"><br>
-
-            <label for="complemento_pf">Complemento:</label>
-            <input type="text" name="complemento_pf" placeholder="Opcional"><br>
         </div>
 
         <!-- Campos específicos para Pessoa Jurídica -->
@@ -253,52 +160,6 @@ else {
             <label for="telefone_pj">Telefone:</label>
             <input type="text" name="telefone_pj_ddd" placeholder="DDD" maxlength="3">
             <input type="text" name="telefone_pj_numero" placeholder="Número" maxlength="11"><br>
-
-            <label for="estado_pj">Estado:</label>
-            <select name="estado_pj">
-                <option value="AC">Acre (AC)</option>
-                <option value="AL">Alagoas (AL)</option>
-                <option value="AP">Amapá (AP)</option>
-                <option value="AM">Amazonas (AM)</option>
-                <option value="BA">Bahia (BA)</option>
-                <option value="CE">Ceará (CE)</option>
-                <option value="DF">Distrito Federal (DF)</option>
-                <option value="ES">Espírito Santo (ES)</option>
-                <option value="GO">Goiás (GO)</option>
-                <option value="MA">Maranhão (MA)</option>
-                <option value="MT">Mato Grosso (MT)</option>
-                <option value="MS">Mato Grosso do Sul (MS)</option>
-                <option value="MG">Minas Gerais (MG)</option>
-                <option value="PA">Pará (PA)</option>
-                <option value="PB">Paraíba (PB)</option>
-                <option value="PR">Paraná (PR)</option>
-                <option value="PE">Pernambuco (PE)</option>
-                <option value="PI">Piauí (PI)</option>
-                <option value="RJ">Rio de Janeiro (RJ)</option>
-                <option value="RN">Rio Grande do Norte (RN)</option>
-                <option value="RS">Rio Grande do Sul (RS)</option>
-                <option value="RO">Rondônia (RO)</option>
-                <option value="RR">Roraima (RR)</option>
-                <option value="SC">Santa Catarina (SC)</option>
-                <option value="SP">São Paulo (SP)</option>
-                <option value="SE">Sergipe (SE)</option>
-                <option value="TO">Tocantins (TO)</option>
-            </select><br>
-
-            <label for="bairro_pj">Bairro:</label>
-            <input type="text" name="bairro_pj"><br>
-
-            <label for="cep_pj">CEP:</label>
-            <input type="text" name="cep_pj"><br>
-
-            <label for="logradouro_pj">Logradouro:</label>
-            <input type="text" name="logradouro_pj"><br>
-
-            <label for="numero_pj">Número:</label>
-            <input type="text" name="numero_pj"><br>
-
-            <label for="complemento_pj">Complemento:</label>
-            <input type="text" name="complemento_pj" placeholder="Opcional"><br>
         </div>
 
         <!-- Campos de email e senha (inicialmente ocultos) -->
@@ -308,21 +169,12 @@ else {
 
             <label for="senha">Senha:</label>
             <input type="password" name="senha" required><br>
-
-            <!--<label for="confirmar_senha">Confirmar Senha:</label>
-            <input type="password" name="confirmar_senha" id="confirmar_senha" required><br>-->
         </div>
 
         <input type="submit" value="Cadastrar" id="cadastrar" style="display: none;">
     </form>
 
     <script>
-        // a masca nao está funcionando.
-        //$(document).ready(function(){
-                //$("#cep_pf").mask("00 000-000");
-            //});
-
-
         // Função para mostrar/ocultar campos com base no tipo de pessoa selecionado
         function showFields(id) {
             // Oculta todos os campos
@@ -341,20 +193,7 @@ else {
                 document.getElementById('email_senha_fields').style.display = 'block';
             }
         }
-
-        //function validarSenha() {
-            //var senha = document.getElementById("senha").value;
-            //var confirmarSenha = document.getElementById("confirmar_senha").value;
-
-            //if (senha !== confirmarSenha) {
-                //alert("As senhas não coincidem. Por favor, verifique.");
-                //return false;
-            //}
-
-            //return true;
-        //}
     </script>
 <?php include 'rodape.html' ?>
 </body>
 </html>
-
--- login.php
+++ login.php
@@ -47,14 +47,13 @@ include 'conexao.php';
 			<div class="row">
 				<div class="col-sm-4 col-sm-offset-4">
 					<h2 class="LU">Login de Usuário</h2>
-					<form name="frmusuario" method="post" action="validacao_usuario.php">
 						<div class="form-group">
 							<label for="email">Email</label>
-							 <input name="txtemail" type="email" class="form-control" required id="email">
+							 <input name="email" type="email" class="form-control" required id="email">
 						</div>
 						<div class="form-group">
 							<label for="senha">Senha</label>
-							<input name="txtsenha" type="password" class="form-control" required id="senha">
+							<input name="senha" type="password" class="form-control" required id="senha">
 						</div>
 							
 						<button type="submit" class="btn btn-lg btn-default">
@@ -63,8 +62,7 @@ include 'conexao.php';
 					
 						<button type="submit" class="btn btn-lg btn-link">
 							Ainda não sou cadastrado
-						</button>
-					</form>	
+						</button>	
 				</div>
 			</div>
 		</div>
